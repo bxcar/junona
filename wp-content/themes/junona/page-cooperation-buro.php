baa0ec48daaa20d827fb69586302efc0bdb76101
bxcar <malanchukdima@mail.ru>
2017-02-25 18:03:59 +0200

    some changes

---
+++ wp-content/themes/junona/page-cooperation-buro.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Template Name: cooperation-buro
+ */
+?>
+
+<?php get_header(); ?>
+
+<!-- Content -->
+<main>
+    <section class="top-block inside-top cooperation-top">
+        <h1>Сотрудничество</h1>
+        <div class="breadcrumb">
+            <ul>
+                <li><a href="index.html">Главная</a></li>
+                <li><a href="cooperation.html">Сотрудничество</a></li>
+                <li><span>Бюро переводов</span></li>
+            </ul>
+        </div>
+    </section>
+    <section class="chess-layaut">
+        <div class="chess-row">
+            <div class="item light">
+                <div class="item-wrap">
+                    <div class="title">Тур компаниям</div>
+                    <p>Бюро переводов “Юнона” было организовано в 2006 году и с тех пор компания сумела стать одним из лидеров на рынке переводов Киева и Украины. Сейчас бюро предоставляет свои услуги перевода украинским и иностранным компаниям, банкам и государственным организациям.</p>
+                    <p>Наша компания предоставляет полный комплекс услуг перевода,что избавит Вас от необходимости обращаться в несколько организаций: Письменный перевод официальных документов, юридических документов, бизнес документов, перевод текстов различной тематики; Вычитка, верстка документов и текстов;
+                        Проставление апостиля и легализация документов в МИДе, Министерстве Юстиций, Министерстве образования</p>
+                    <p>Проставление штампа главного управления юстиции в г. Киев; Печать в Министерстве Здравоохранения; Нотариальное заверение перевода документов; Заверение перевода документов печатью бюро переводов; Курьерская доставка и доставка онлайн; Редактирование перевода носителем языка; Переполучение, легализация, апостиль документов старого образца на территории
+                        стран СНД;</p>
+                </div>
+            </div>
+            <div class="item img"><img src="img/buro.jpg" alt=""></div>
+        </div>
+    </section>
+    <section class="do-order cooperation">
+        <div class="wrap">
+            <div class="title">Свяжитесь с нами</div>
+            <div class="order-form">
+                <div class="item">
+                    <input type="text" placeholder="Ваше имя*">
+                    <input type="text" placeholder="Тип бизнеса">
+                </div>
+                <div class="item">
+                    <input type="tel" placeholder="Номер телефона*">
+                    <input type="email" placeholder="E-mail">
+                </div>
+            </div>
+            <input type="submit">
+
+        </div>
+    </section>
+</main>
+<!-- End content -->
+
+<?php get_footer(); ?>
