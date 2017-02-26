74e3e363d0c7f32e6c2dfdfb4a337ed008f3f24f
bxcar <malanchukdima@mail.ru>
2017-02-27 00:48:08 +0200

    some changes

---
+++ wp-content/themes/junona/page-review.php
@@ -0,0 +1,215 @@
+<?php
+/**
+ * Template Name: reviews
+ */
+?>
+
+<?php get_header(); ?>
+
+<!-- Content -->
+<main>
+    <section class="top-block inside-top review-top">
+        <h1>Отзывы клиентов</h1>
+        <div class="breadcrumb">
+            <ul>
+                <li><a href="index.html">Главная</a></li>
+                <li><a href="about-service.html">О сервисе</a></li>
+                <li><span>Отзывы клиентов</span></li>
+            </ul>
+        </div>
+    </section>
+    <section class="client-review">
+        <div class="wrap">
+            <div class="title">Отзывы</div>
+            <div class="rev-item">
+                <img src="img/client-1.png" alt="" class="client">
+                <div class="text">
+                    <div class="top">
+                        <div class="name">Валерий Ранский</div>
+                        <div class="date">26 июня 2016</div>
+                        <p>Мы с вами реализовали один заказ. Общее впечатление от вас хорошее. С учетом того, что это - Украина, можно смело ставить отлично. Удачи. Успехов. При необходимости закажу вашу услугу.</p>
+                    </div>
+                    <div class="bottom">
+                        <div class="rating">8,8</div>
+                        <div class="field">
+                            <div class="field-name">Скорость работы</div>
+                            <span></span>
+                            <div class="field-prop">8,0</div>
+                        </div>
+                        <div class="field">
+                            <div class="field-name">Точность работы</div>
+                            <span></span>
+                            <div class="field-prop">9,5</div>
+                        </div>
+                        <div class="field">
+                            <div class="field-name">Стоимость работы</div>
+                            <span></span>
+                            <div class="field-prop">9,0</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="rev-item">
+                <img src="img/client-2.png" alt="" class="client">
+                <div class="text">
+                    <div class="top">
+                        <div class="name">Виктория Орел</div>
+                        <div class="date">5 июня 2016</div>
+                        <p>Мне понравился Ваш сервис. Мне сделали перевод в течение двух часов. Спасибо за оперативность и гибкость.</p>
+                    </div>
+                    <div class="bottom">
+                        <div class="rating">9,0</div>
+                        <div class="field">
+                            <div class="field-name">Скорость работы</div>
+                            <span></span>
+                            <div class="field-prop">8,5</div>
+                        </div>
+                        <div class="field">
+                            <div class="field-name">Точность работы</div>
+                            <span></span>
+                            <div class="field-prop">8,5</div>
+                        </div>
+                        <div class="field">
+                            <div class="field-name">Стоимость работы</div>
+                            <span></span>
+                            <div class="field-prop">10,0</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="rev-item">
+                <img src="img/client-3.png" alt="" class="client">
+                <div class="text">
+                    <div class="top">
+                        <div class="name">Greasle Mark Eugene </div>
+                        <div class="date">5 июня 2016</div>
+                        <p>Thanks a lot for your help. It is very hard to find the people who can help with the advice. You are awesome guys!</p>
+                    </div>
+                    <div class="bottom">
+                        <div class="rating">9,3</div>
+                        <div class="field">
+                            <div class="field-name">Скорость работы</div>
+                            <span></span>
+                            <div class="field-prop">10,0</div>
+                        </div>
+                        <div class="field">
+                            <div class="field-name">Точность работы</div>
+                            <span></span>
+                            <div class="field-prop">8,5</div>
+                        </div>
+                        <div class="field">
+                            <div class="field-name">Стоимость работы</div>
+                            <span></span>
+                            <div class="field-prop">9,5</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="rev-item">
+                <img src="img/client-4.png" alt="" class="client">
+                <div class="text">
+                    <div class="top">
+                        <div class="name">Ростислав Которий</div>
+                        <div class="date">Май 2014</div>
+                        <p>Очень рад, что обратился именно к Вам. Очень нравиться Ваша ответственность, оперативность и исполнительность. Еще ни разу Ваша компания не подвела меня. Спасибо Вам огромное.</p>
+                    </div>
+                    <div class="bottom">
+                        <div class="rating">8,8</div>
+                        <div class="field">
+                            <div class="field-name">Скорость работы</div>
+                            <span></span>
+                            <div class="field-prop">8,0</div>
+                        </div>
+                        <div class="field">
+                            <div class="field-name">Точность работы</div>
+                            <span></span>
+                            <div class="field-prop">9,5</div>
+                        </div>
+                        <div class="field">
+                            <div class="field-name">Стоимость работы</div>
+                            <span></span>
+                            <div class="field-prop">9,0</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="rev-item">
+                <img src="img/client-5.png" alt="" class="client">
+                <div class="text">
+                    <div class="top">
+                        <div class="name">Тревелина</div>
+                        <div class="date">Май 2014</div>
+                        <p>Не смотря на то, что начали пользоваться услугами Вашего Бюро переводов совсем недавно, нам нравится то, как сотрудники профессионально и ответственно относятся ко всему. Переводы выполняются быстро, аккуратно и качественно, а самое главное без задержек! </p>
+                    </div>
+                    <div class="bottom">
+                        <div class="rating">9,8</div>
+                        <div class="field">
+                            <div class="field-name">Скорость работы</div>
+                            <span></span>
+                            <div class="field-prop">10,0</div>
+                        </div>
+                        <div class="field">
+                            <div class="field-name">Точность работы</div>
+                            <span></span>
+                            <div class="field-prop">9,5</div>
+                        </div>
+                        <div class="field">
+                            <div class="field-name">Стоимость работы</div>
+                            <span></span>
+                            <div class="field-prop">10,0</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="" class="base-btn">+ 10 ОТЗЫВОВ</a>
+        </div>
+    </section>
+    <section class="assessment-work leave-review">
+        <div class="wrap">
+            <div class="title white">Оставить отзыв</div>
+            <div class="assessment-form">
+                <div class="item">
+                    <input type="text" placeholder="Ваше имя*">
+                    <div class="rating-inp">
+                        <p>Сроки выполнения</p>
+                        <div class="ticker-wrap">
+                            <div id="slider" class="ticker">
+                                <input class="number" type="text" id="amount">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <input type="email" placeholder="E-mail">
+                    <div class="rating-inp">
+                        <p>Качество</p>
+                        <div class="ticker-wrap">
+                            <div id="slider-accuracy" class="ticker">
+                                <input class="number" type="text" id="amount-accuracy">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <input type="file" placeholder="Прикрепить фото*">
+                    <div class="rating-inp">
+                        <p>Стоимость</p>
+                        <div class="ticker-wrap">
+                            <div id="slider-price" class="ticker">
+                                <input class="number" type="text" id="amount-price">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item large">
+                    <textarea name="" id="" cols="20" rows="5" placeholder="Комментарии"></textarea>
+                </div>
+            </div>
+            <input type="submit">
+
+        </div>
+    </section>
+</main>
+<!-- End content -->
+
+<?php get_footer(); ?>
