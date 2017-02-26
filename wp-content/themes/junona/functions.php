79164799e595b1d4376b41169667d0c3feb8c4ca
bxcar <malanchukdima@mail.ru>
2017-02-26 17:27:10 +0200

    some changes

--- wp-content/themes/junona/functions.php
+++ wp-content/themes/junona/functions.php
@@ -147,3 +147,19 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+add_action('admin_menu', 'register_my_custom_menu_page');
+function register_my_custom_menu_page()
+{
+    add_menu_page(
+        'custom menu title', 'Контентные разделы', 'manage_options', 'custompage', 'my_custom_menu_page', 'dashicons-format-aside', 21.3
+    );
+    // Add a second submenu to the custom top-level menu:
+    add_submenu_page('custompage', 'Языки', 'Языки', 8, '/edit.php?post_type=language');
+    remove_submenu_page('custompage', 'custompage');
+}
+
+function my_custom_menu_page()
+{
+    echo "<h3>Выберите нужный вам раздел для редактирования, которые отображены слева в подменю данного раздела.</h3>";
+}
---
+++ wp-content/themes/junona/single-language.php
@@ -0,0 +1,212 @@
+
+<?php get_header(); ?>
+
+<!-- Content -->
+<main>
+    <section class="top-block inside-top lang-top">
+        <h1>Языки</h1>
+        <div class="breadcrumb">
+            <ul>
+                <li><a href="index.html">Главная</a></li>
+                <li><a href="lang.html">Языки</a></li>
+                <li><span>Профессиональный английский перевод</span></li>
+            </ul>
+        </div>
+    </section>
+    <section class="chess-layaut">
+        <div class="chess-row">
+            <div class="item light">
+                <div class="item-wrap">
+                    <div class="title">Профессиональный
+                        английский перевод</div>
+                    <p>Профессиональный перевод с английского языка является одной из специализаций нашего бюро переводов.   Наличие штатных переводчиков, специализирующихся по различным тематикам, оптимизирование рабочих процессов и контроля качества, позволяют нам предложить Вам профессиональные английские переводы по приятным расценкам 7 дней в неделю.</p>
+                    <p><span class="bold">Дополнительные услуги:</span></p>
+                    <ul>
+                        <li><p>Вычитка носителем языка: $0.04 за слово;</p></li>
+                        <li><p>Доставка курьером по Киеву – от 30 грн в зависимости от расстояния;</p></li>
+                        <li><p>При заказе на сумму более 2500 грн. доставка бесплатная;</p></li>
+                        <li><p>Доставка по Украине – договорная.</p></li>
+                    </ul>
+                    <p>Готовый перевод заверяется печатью бюро переводов, при необходимости готовый перевод заверяется нотариально.</p>
+                </div>
+            </div>
+            <div class="item img"><img src="img/eng-trans.jpg" alt=""></div>
+        </div>
+    </section>
+    <section class="our-services-translate">
+        <div class="wrap">
+            <div class="title white">Наши услуги переводов включают в себя</div>
+            <div class="our-services-item">
+                <div class="item">
+                    <div class="wrapper">
+                        <img src="img/our-item-1.png" alt="">
+                        <div class="item-title">Профессинальный
+                            менеджмент </div>
+                        <p>Профессиональное ведение каждого проекта</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="wrapper">
+                        <img src="img/our-item-2.png" alt="">
+                        <div class="item-title">Качественный
+                            перевод</div>
+                        <p>Квалифицированным специалистом с опытом перевода в необходимой области</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="wrapper">
+                        <img src="img/our-item-3.png" alt="">
+                        <div class="item-title">Консультации
+                            специалиста</div>
+                        <p>По тематике по требованию</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="wrapper">
+                        <img src="img/our-item-4.png" alt="">
+                        <div class="item-title">Вычитка
+                            носителем языка</div>
+                        <p>Вычитку носителем языка ( по требованию)</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="wrapper">
+                        <img src="img/our-item-5.png" alt="">
+                        <div class="item-title">Клиентский портал</div>
+                        <p>Загрузка проектов, инвойсов и общение с поддержкой</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="wrapper">
+                        <img src="img/our-item-6.png" alt="">
+                        <div class="item-title">Глоссарий</div>
+                        <p>Создание и использование в последующих проектах</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="wrapper">
+                        <img src="img/our-item-7.png" alt="">
+                        <div class="item-title">Гарантия качества</div>
+                        <p>Все исправления бесплатны в течение 10и дней</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="wrapper">
+                        <img src="img/our-item-8.png" alt="">
+                        <div class="item-title">Заголовок</div>
+                        <p>Еще один пункт</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="single-services-price">
+        <div class="wrap">
+            <div class="title">Стоимость услуг перевода английского языка</div>
+            <div class="table-price services-price">
+                <div class="table-row top">
+                    <div class="lang">услуга</div>
+                    <div class="transl-to">Перевод
+                        стандартного
+                        документа</div>
+                    <div class="transl-to">Перевод текста
+                        повышенной
+                        сложности</div>
+                    <div class="transl-to">Перевод текста
+                        повышенной
+                        сложности</div>
+                </div>
+                <div class="table-row">
+                    <div class="lang">
+                        <p>Перевод официального документа</p>
+                    </div>
+                    <div class="translate-price">60 грн</div>
+                    <div class="translate-price">78 грн</div>
+                    <div class="translate-price">90 грн</div>
+                </div>
+                <div class="table-row">
+                    <div class="lang">
+                        <p>Перевод текста</p>
+                    </div>
+                    <div class="translate-price">60 грн</div>
+                    <div class="translate-price">78 грн</div>
+                    <div class="translate-price">90 грн</div>
+                </div>
+                <div class="table-row">
+                    <div class="lang">
+                        <p>Вычитка готового перевода </p>
+                    </div>
+                    <div class="translate-price"><span>от</span> 40 грн</div>
+                    <div class="translate-price"><span>от</span> 40 грн</div>
+                    <div class="translate-price"><span>от</span> 40 грн</div>
+                </div>
+                <div class="table-row">
+                    <div class="lang">
+                        <p>Заверение перевода подписью
+                            нотариуса</p>
+                    </div>
+                    <div class="translate-price">120 грн</div>
+                    <div class="translate-price">120 грн</div>
+                    <div class="translate-price">120 грн</div>
+                </div>
+                <div class="table-row">
+                    <div class="lang">
+                        <p>Заверение перевода печатью бюро
+                            переводов</p>
+                    </div>
+                    <div class="translate-price">15 грн</div>
+                    <div class="translate-price">15 грн</div>
+                    <div class="translate-price">15 грн</div>
+                </div>
+                <div class="table-row">
+                    <div class="lang">
+                        <p>Устный перевод на встречах</p>
+                    </div>
+                    <div class="translate-price">145 грн</div>
+                    <div class="translate-price">200 грн</div>
+                    <div class="translate-price">250 грн</div>
+                </div>
+                <div class="table-row">
+                    <div class="lang">
+                        <p>Устный перевод по Скайпу</p>
+                    </div>
+                    <div class="translate-price">135 грн</div>
+                    <div class="translate-price">195 грн</div>
+                    <div class="translate-price">255 грн</div>
+                </div>
+            </div>
+            <div class="text">
+                <p class="subtitle">Примечание:</p>
+                <p>Стоимость указана в гривнах за 1800 знаков c пробелами. </p>
+                <p><span class="bold">Перевод стандартного документа:</span></p>
+                <p>Свидетельства о рождении, браке, разводе, смерти; справки о несудимости, справки с места работы, справки о зарплате, справки о состоянии счета в банке, дипломы (без приложения).</p>
+                <p><span class="bold">Перевод текста повышенной сложности:</span></p>
+                <p>Договора, уставы компаний, узкоспециальные тексты и пр. документы, имеющие оригинальные неповторяющийся текст.</p>
+                <p><span class="bold">Перевод текста высокой сложности:</span></p>
+                <p>Узкоспециализированные тексты, тексты инновационного характера , технический перевод, перевод чертежей, инструкций т.д.</p>
+                <p><span class="bold">Минимальный заказ – 1 час. Надбавка за срочность (в течение 2х часов): +100% </span></p>
+            </div>
+        </div>
+    </section>
+    <section class="do-order">
+        <div class="wrap">
+            <div class="title">Заказать перевод</div>
+            <div class="order-form">
+                <div class="item">
+                    <input type="text" placeholder="Ваше имя*">
+                    <input type="tel" placeholder="Номер телефона*">
+                    <input type="email" placeholder="E-mail">
+                </div>
+                <div class="item">
+                    <input type="file" placeholder="Номер телефона*">
+                    <textarea name="" id="" cols="20" rows="5" placeholder="Комментарии"></textarea>
+                </div>
+            </div>
+            <input type="submit">
+
+        </div>
+    </section>
+</main>
+<!-- End content -->
+
+<?php get_footer(); ?>
